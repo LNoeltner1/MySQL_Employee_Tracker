18003e318c6d1d8e05321dfa7a7bbb61fffaa87d
Lauren Noeltner <laurennoeltner@gmail.com>
2020-10-04 20:05:38 -0400

    Got initial question/option list working

--- index.js
+++ index.js
@@ -3,8 +3,28 @@ const mysql = require("mysql");
 const cTable = require("console.table");
 const fs = require("fs");
 
+const initQuestion = [
+  {
+    type: "list",
+    message: "What would you like to do?",
+    name: "mainAction",
+    choices: [
+      "Add Employee",
+      "Add Role",
+      "Add Department",
+      "View Employees",
+      "View Roles",
+      "View Departments",
+      "Update Employee Role",
+    ],
+  },
+];
+
 inquirer
-  .prompt({ type: "input", message: "Figg'rit'oot", name: "response" })
+  .prompt(initQuestion)
   .then(function (response) {
     console.log(response);
+  })
+  .catch((err) => {
+    console.log(err);
   });
