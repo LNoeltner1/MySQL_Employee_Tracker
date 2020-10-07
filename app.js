24ce52d7546680715fbe6fa2a54f8b4149d8c2a6
Lauren Noeltner <laurennoeltner@gmail.com>
2020-10-06 23:33:09 -0400

    Got all but removeEmployees working

--- app.js
+++ app.js
@@ -100,7 +100,6 @@ function addEmployee() {
         },
         function (err, data) {
           if (err) throw err;
-          console.log(data);
         }
       );
       var query3 =
@@ -112,113 +111,159 @@ function addEmployee() {
         "INNER JOIN employee_db.departments d ON r.department_id = d.id";
       connection.query(query3, function (err, data) {
         if (err) throw err;
+        console.log("=======================================");
         console.table(data);
-        // startApp();
       });
+      inquirer
+        .prompt({
+          name: "back",
+          message: "Go back:",
+          choices: ["BACK"],
+        })
+        .then(function (response) {
+          startApp();
+        });
     })
+
     .catch((err) => {
       console.log(err);
     });
 }
 
-// function addRole() {
-//   const roleAssignmentPrompt = [
-//     {
-//       type: "number",
-//       message: "What role would you like to create?",
-//       name: "addrole",
-//     },
-//   ];
-//   inquirer.prompt(roleAssignmentPrompt).then(function (response) {
-//     var query8 = "INSERT INTO roles SET ?";
-//     connection.query(query8, { role_name: response.name }, function (
-//       err,
-//       data
-//     ) {
-//       if (err) throw err;
-//     });
-//     var query9 = "SELECT * FROM roles";
-//     connection
-//       .query(query9, function (err, data) {
-//         if (err) throw err;
-//         console.table(data);
-//         // startApp();
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//       });
-//   });
-// }
+function addRole() {
+  const roleAssignmentPrompt = [
+    {
+      type: "input",
+      message: "What role would you like to create?",
+      name: "addrole",
+    },
+    {
+      type: "number",
+      message: "What is the salary at this role?",
+      name: "salary",
+    },
+    {
+      type: "input",
+      message: "What is the department ID of this role?",
+      name: "id",
+    },
+  ];
+  inquirer.prompt(roleAssignmentPrompt).then(function (response) {
+    var query8 = "INSERT INTO roles SET ?";
+    connection.query(
+      query8,
+      {
+        role_name: response.addrole,
+        salary: response.salary,
+        department_id: response.id,
+      },
+      function (err, data) {
+        if (err) throw err;
+      }
+    );
+    var query9 = "SELECT * FROM roles";
+    connection.query(query9, function (err, data) {
+      if (err) throw err;
+      console.log("=======================================");
+      console.table(data);
+      // startApp();
+    });
+    inquirer
+      .prompt({
+        name: "back",
+        message: "Go Back:",
+        choices: ["BACK"],
+      })
+      .then(function (response) {
+        startApp();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+}
 
-// function addDepartment() {
-//   const addDeptOptions = [
-//     {
-//       type: "input",
-//       message: "What department would you like to add?",
-//       name: "adddepartment",
-//     },
-//   ];
-//   inquirer.prompt(addDeptOptions).then(function (response) {
-//     var query10 = "INSERT INTO departments SET ?";
-//     connection.query(
-//       query10,
-//       {
-//         department_name: response.name,
-//       },
-//       function (err, data) {
-//         if (err) throw err;
-//         console.table(data);
-//       }
-//     );
-//   });
-// }
+function addDepartment() {
+  const addDeptOptions = [
+    {
+      type: "input",
+      message: "What department would you like to add?",
+      name: "adddepartment",
+    },
+  ];
+  inquirer.prompt(addDeptOptions).then(function (response) {
+    var query10 = "INSERT INTO departments SET ?";
+    connection.query(
+      query10,
+      {
+        department_name: response.adddepartment,
+      },
+      function (err, data) {
+        if (err) throw err;
+        console.log("=======================================");
+        console.table(data);
+      }
+    );
+    inquirer
+      .prompt({
+        name: "back",
+        message: "Go Back:",
+        choices: ["BACK"],
+      })
+      .then(function (response) {
+        startApp();
+      });
+  });
+}
 
-// function viewEmployees() {
-//   var query7 =
-//     "SELECT e.first_name, e.last_name, r.role_name, r.salary, e.manager_id, m.first_name AS manager_firstname, m.last_name AS manager_lastname, r.department_id ";
-//   query7 += "FROM employee_db.employees e ";
-//   query7 += "INNER JOIN employee_db.employees m ON e.manager_id = m.id ";
-//   query7 += "INNER JOIN employee_db.roles r ON e.role_id = r.id ";
-//   query7 += "INNER JOIN employee_db.departments d ON r.department_id = d.id";
-//   connection.query(query7, function (err, data) {
-//     if (err) throw err;
-//     console.table(data);
-//   });
+function viewEmployees() {
+  var query7 =
+    "SELECT e.first_name, e.last_name, r.role_name, r.salary, e.manager_id, m.first_name AS manager_firstname, m.last_name AS manager_lastname, r.department_id ";
+  query7 += "FROM employee_db.employees e ";
+  query7 += "INNER JOIN employee_db.employees m ON e.manager_id = m.id ";
+  query7 += "INNER JOIN employee_db.roles r ON e.role_id = r.id ";
+  query7 += "INNER JOIN employee_db.departments d ON r.department_id = d.id";
+  connection.query(query7, function (err, data) {
+    if (err) throw err;
+    console.log("=======================================");
+    console.table(data);
+  });
 
-//   inquirer
-//     .prompt({ name: "back", message: "", choices: "BACK" })
-//     .then(function (response) {
-//       // startApp();
-//     });
-// }
+  inquirer
+    .prompt({ name: "back", message: "Go Back", choices: "BACK" })
+    .then(function (response) {
+      startApp();
+    });
+}
 
-//   function viewRoles() {
-//     var query5 = "SELECT * FROM roles";
-//     connection.query(query5, function (err, data) {
-//       if (err) throw err;
-//       console.table(data);
-//     });
+function viewRoles() {
+  var query5 = "SELECT * FROM roles";
+  connection.query(query5, function (err, data) {
+    if (err) throw err;
+    console.log("=======================================");
+    console.table(data);
+  });
 
-//     inquirer
-//       .prompt({ name: "back", message: "", choices: "BACK" })
-//       .then(function (response) {
-//         // startApp();
-//       });
-//   }
+  inquirer
+    .prompt({ name: "back", message: "Go Back", choices: "BACK" })
+    .then(function (response) {
+      startApp();
+    });
+}
 
-//   function viewDepartments() {
-//     var query6 = "SELECT * FROM departments";
-//     connection.query(query6, function (err, data) {
-//       if (err) throw err;
-//       console.table(data);
-//     });
+function viewDepartments() {
+  var query6 = "SELECT * FROM departments";
+  connection.query(query6, function (err, data) {
+    if (err) throw err;
+    console.table(data);
+  });
 
-//     inquirer
-//       .prompt({ name: "back", message: "", choices: "BACK" })
-//       .then(function (response) {
-//         // startApp();
-//       });
-//   }
+  inquirer
+    .prompt({ name: "back", message: "Go Back", choices: "BACK" })
+    .then(function (response) {
+      startApp();
+    });
+}
 
 //   function updateRole() {
 //     const roleAssignmentPrompt = [
